15d9c14439ee2589f8edeee4380bfcb57e1c035f
MohdThaha <thahad2@gmail.com>
2024-08-05 11:52:36 +0000

    org

--- controller/settingController.js
+++ controller/settingController.js
@@ -23,3 +23,39 @@ exports.getCurrency = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+
+
+exports.getAllTimeZones = async (req, res) => {
+  try {
+    const timeZones = [
+      {"value": "UTC", "label": "Coordinated Universal Time (UTC)"},
+      {"value": "GMT", "label": "Greenwich Mean Time (GMT)"},
+      {"value": "PST", "label": "Pacific Standard Time (PST)"},
+      {"value": "MST", "label": "Mountain Standard Time (MST)"},
+      {"value": "CST", "label": "Central Standard Time (CST)"},
+      {"value": "EST", "label": "Eastern Standard Time (EST)"},
+      {"value": "IST", "label": "India Standard Time (IST)"},
+      {"value": "CET", "label": "Central European Time (CET)"},
+      {"value": "EET", "label": "Eastern European Time (EET)"},
+      {"value": "JST", "label": "Japan Standard Time (JST)"},
+      {"value": "AEST", "label": "Australian Eastern Standard Time (AEST)"},
+      {"value": "ACST", "label": "Australian Central Standard Time (ACST)"},
+      {"value": "AWST", "label": "Australian Western Standard Time (AWST)"},
+      {"value": "NZST", "label": "New Zealand Standard Time (NZST)"},
+      {"value": "HST", "label": "Hawaii-Aleutian Standard Time (HST)"},
+      {"value": "AKST", "label": "Alaska Standard Time (AKST)"},
+      {"value": "WET", "label": "Western European Time (WET)"},
+      {"value": "WEST", "label": "Western European Summer Time (WEST)"},
+      {"value": "CEST", "label": "Central European Summer Time (CEST)"},
+      {"value": "EEST", "label": "Eastern European Summer Time (EEST)"},
+      {"value": "BST", "label": "British Summer Time (BST)"}
+    ];
+
+    res.status(200).json(timeZones);
+  } catch (error) {
+    console.error("Error fetching time zones:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
