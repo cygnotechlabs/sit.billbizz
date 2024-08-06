490c544fcb490ac693e5688ff0a6624802f15137
amanrasheed <amanrasheedpro@gmail.com>
2024-08-06 10:46:39 +0530

    ProtIssue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Change localhost to 0.0.0.0 to listen on all network interfaces
+    port: 5173,
+    host: "0.0.0.0", // Listen on all network interfaces
   },
 });
