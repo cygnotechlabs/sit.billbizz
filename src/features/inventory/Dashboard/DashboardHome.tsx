9d9e3719fac3a30605ef5df011146c256323ea45
MohdThaha <thahad2@gmail.com>
2024-08-24 18:43:18 +0000

    inventory small updates

--- src/features/inventory/Dashboard/DashboardHome.tsx
+++ src/features/inventory/Dashboard/DashboardHome.tsx
@@ -1,4 +1,8 @@
-import BarCharts from "./Barcharts";
+import AvaragePurchase from "../../accountant/Dashboard/AvaragePurchase";
+import CustomersRetentionRate from "../../accountant/Dashboard/CustomersRetentionRate";
+import RepeatPurchaseRate from "../../accountant/Dashboard/RepeatPurchaseRate";
+import TopCustomers from "../../accountant/Dashboard/TopCustomers";
+
 import Cards from "./Cards";
 
 type Props = {};
@@ -7,7 +11,7 @@ function DashboardHome({}: Props) {
   return (
     <>
       <div className="pl-5 pr-5 w-[100%]">
-        <Cards />
+        <Cards/>
       </div>
       <div className="p-5">
         <div className="w-[100%] p-3 bg-gray-100"></div>
@@ -15,24 +19,27 @@ function DashboardHome({}: Props) {
           <div className="w-[80%]"></div>
         </div>
       </div>
-      <div className="flex justify-between">
-        <div className="">
-          <BarCharts />
-        </div>
-        <div className="">
-          <BarCharts />
-        </div>
-      </div>
+     
       <div>
-        <div className="">
-          <BarCharts />
-        </div>
-        <div className="">
-          <BarCharts />
+      <div className="grid grid-cols-3 gap-5">
+          <div className="flex justify-center ">
+            <TopCustomers />
+          </div>
+          <div className="col-span-2 flex justify-center">
+            <CustomersRetentionRate />
+          </div>
+          <div className="col-span-2 flex justify-center ">
+            <RepeatPurchaseRate />
+          </div>
+          <div className=" flex justify-center">
+            <AvaragePurchase />
+          </div>
         </div>
       </div>
-    </>
+
+      </>
   );
 }
+ 
 
 export default DashboardHome;
