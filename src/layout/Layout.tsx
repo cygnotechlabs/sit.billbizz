1f6958f410c4c0f216cd6e3a847b29c0652a432a
Justin <justinvarghesemicheal@gmail.com>
2024-09-10 16:29:37 +0530

    customerinfoui

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,26 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header/Header";
 import SideBar from "./SideBar/SideBar";
 import SubHeader from "./SubHeader/SubHeader";
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useLocation } from "react-router-dom";
+ 
 type Props = {
   children: React.ReactNode;
 };
-
+ 
 const Layout = ({}: Props) => {
+  const location=useLocation()
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
+  const [subHeaderView,setSubHeaderView]=useState(false)
+ 
+  useEffect(()=>{
+    setSubHeaderView(location.pathname.endsWith('info'))
+  },[location])
   return (
     <div className="flex">
       <SideBar activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
       <div className="w-[100%]">
         <Header />
-        <SubHeader activeIndex={activeIndex} />
+        {!subHeaderView&&<SubHeader activeIndex={activeIndex} />}
         <Outlet />
       </div>
     </div>
   );
 };
-
+ 
 export default Layout;
