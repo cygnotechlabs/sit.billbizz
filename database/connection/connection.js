b3fdc048eda62b4f1af0c01fadab304cec879630
MohdThaha <thahad2@gmail.com>
2024-09-17 11:48:49 +0530

    org

--- database/connection/connection.js
+++ database/connection/connection.js
@@ -10,3 +10,38 @@ mongoose.connect(DB)
 })
 
 
+
+
+
+
+// const AWS = require('aws-sdk');
+// const secretsManager = new AWS.SecretsManager({
+//   region: 'ap-south-1' 
+// });
+
+// async function getSecret(secretName) {
+//   try {
+//     const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+//     if (data.SecretString) {
+//       return JSON.parse(data.SecretString);
+//     }
+//   } catch (err) {
+//     console.error("Error fetching secret:", err);
+//     throw err;
+//   }
+// }
+
+
+// // Example usage
+// (async () => {
+//     const DB = await getSecret('your-secret-name');
+//   })();
+
+
+
+//   mongoose.connect(DB)
+//   .then(()=>{
+//       console.log("📡...BillBizz Database Connected Succesfully...📡");
+//   }).catch((error)=>{
+//       console.log(`Database error ${error}`);
+//   })
