e72aaaf363f09109d61b53a24ef27f19b033d5a7
Alwin-Cygno <alwinsathish260196@gmial.com>
2024-09-17 21:56:28 +0530

    header updates

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,35 +1,38 @@
-import { Link } from "react-router-dom";
-import Button from "../../Components/Button";
+import { Link, useNavigate } from "react-router-dom";
 import SearchBar from "../../Components/SearchBar";
 import SettingsIcons from "../../assets/icons/SettingsIcon";
 import Notification from "./HeaderIcons/Notification";
 import RefferEarn from "./HeaderIcons/RefferEarn";
 import Organization from "./HeaderIcons/Organization";
 import { useState } from "react";
-
+import viewAppsIcon from "../../assets/Images/Frame 629925.png"
 type Props = {};
 
 const Header = ({}: Props) => {
+  const navigate=useNavigate();
+  const handleNavigate =()=>{
+navigate("/landing")
+  }
   const [searchValue, setSearchValue] = useState<string>("");
   return (
     <div
-      className="p-4 flex items-center gap-4 w-full border-b-slate-400 border-y-orange-200"
+      className="p-4 flex items-center gap-2 w-full border-b-slate-400 border-y-orange-200"
       style={{ borderBottom: "1.5px solid rgba(28, 28, 28, 0.1)" }}
     >
-      <div className="flex-grow">
+      <div className="w-[68%]">
         <SearchBar
           placeholder="Serach"
           searchValue={searchValue}
           onSearchChange={setSearchValue}
         />
       </div>
-      <span className="text-xs font-semibold text-gray-700 whitespace-nowrap">
-        Trial expires in 14 days
+      <div className="flex ms-8 justify-center items-center gap-2 cursor-pointer" onClick={handleNavigate}>
+      <img src={viewAppsIcon} alt="" />
+      <span className="text-xs font-semibold text-dropdownText whitespace-nowrap">
+        View Apps
       </span>
+      </div>
       <div className="flex items-center gap-4 ml-auto">
-        <Button variant="tertiary" size="sm">
-          <p className="text-xs  font-medium">Subscribe</p>
-        </Button>
         <select
           className="border border-outlineButton px-[0.625rem] py-2 text-outlineButton rounded-md bg-orange-100 text-xs font-medium     
 "
