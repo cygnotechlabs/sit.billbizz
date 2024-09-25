6bbf3a2427a540cfc3e54ff0d3ad69be8de4be30
MohdThaha <thahad2@gmail.com>
2024-09-25 20:04:26 +0530

    auth

---
+++ database/model/activityLog.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const activityLogSchema = new mongoose.Schema({
+  userName: { type: String},
+  activity: { type: String },
+  timestamp: { type: String },
+  reqBody: { type: String },
+});
+
+module.exports = mongoose.model('ActivityLog', activityLogSchema);
