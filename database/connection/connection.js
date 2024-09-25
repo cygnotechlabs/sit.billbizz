f97739df5d9a18e27aee712cae00172952e45f18
MohdThaha <thahad2@gmail.com>
2024-09-25 16:28:33 +0530

    env

--- database/connection/connection.js
+++ database/connection/connection.js
@@ -1,75 +1,8 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const DB = process.env.DATABASE
+const DB = process.env.DATABASE
 
-// mongoose.connect(DB)
-// .then(()=>{
-//     console.log("📡...BillBizz Database Connected Succesfully...📡");
-// }).catch((error)=>{
-//     console.log(`Database error ${error}`);
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const mongoose = require('mongoose');
-const AWS = require('aws-sdk');
-
-// Configure AWS SDK with region
-const region = 'ap-south-1'; // Your AWS region
-AWS.config.update({ region });
-
-// Create an instance of the SSM (Systems Manager) client
-const ssm = new AWS.SSM();
-
-async function getDbCredentials() {
-  try {
-    // Fetch the parameter from AWS Parameter Store (with decryption enabled)
-    const params = {
-      Name: 'billbizz_DB1', // Replace with your parameter name
-      WithDecryption: true, // Ensure KMS decryption is used
-    };
-
-    const result = await ssm.getParameter(params).promise();
-    const credentials = result.Parameter.Value;
-    console.log('Fetched credentials:', credentials);
-    
-    
-    return credentials.dbUri;  
-  } catch (error) {
-    console.error('Error fetching credentials from Parameter Store:', error);
-    throw error;
-  }
-}
-
-async function connectToDatabase() {
-  try {
-    const dbUri = await getDbCredentials();
-    console.log(`Fetched MongoDB URI: ${dbUri}`);
-
-
-    mongoose.connect(dbUri)
+mongoose.connect(DB)
 .then(()=>{
     console.log("📡...BillBizz Database Connected Succesfully...📡");
 }).catch((error)=>{
@@ -78,20 +11,80 @@ async function connectToDatabase() {
 
 
 
-    // Now use dbUri to connect to your MongoDB database
-    // await mongoose.connect(dbUri, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
 
-    console.log("📡...BillBizz Database Connected Successfully...📡");
-  } catch (error) {
-    console.log(`Database connection error: ${error}`);
-  }
-}
 
-// Initialize database connection
-connectToDatabase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const AWS = require('aws-sdk');
+
+// // Configure AWS SDK with region
+// const region = 'ap-south-1'; // Your AWS region
+// AWS.config.update({ region });
+
+// // Create an instance of the SSM (Systems Manager) client
+// const ssm = new AWS.SSM();
+
+// async function getDbCredentials() {
+//   try {
+//     // Fetch the parameter from AWS Parameter Store (with decryption enabled)
+//     const params = {
+//       Name: 'billbizz_DB1', // Replace with your parameter name
+//       WithDecryption: true, // Ensure KMS decryption is used
+//     };
+
+//     const result = await ssm.getParameter(params).promise();
+//     const credentials = result.Parameter.Value;
+//     console.log('Fetched credentials:', credentials);
+    
+    
+//     return credentials.dbUri;  
+//   } catch (error) {
+//     console.error('Error fetching credentials from Parameter Store:', error);
+//     throw error;
+//   }
+// }
+
+// async function connectToDatabase() {
+//   try {
+//     const dbUri = await getDbCredentials();
+//     console.log(`Fetched MongoDB URI: ${dbUri}`);
+
+
+//     mongoose.connect(dbUri)
+// .then(()=>{
+//     console.log("📡...BillBizz Database Connected Succesfully...📡");
+// }).catch((error)=>{
+//     console.log(`Database error ${error}`);
+// })
+
+
+//     console.log("📡...BillBizz Database Connected Successfully...📡");
+//   } catch (error) {
+//     console.log(`Database connection error: ${error}`);
+//   }
+// }
+
+// // Initialize database connection
+// connectToDatabase();
 
 
 
